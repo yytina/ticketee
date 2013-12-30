9da14457eaf2b30d51b77137a00644a4c95449cc
yytina <yang.hyejin@gmail.com>
2013-12-30 08:04:13 +0800

    Restrict access to project actions to admin only

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -8,4 +8,31 @@ describe ProjectsController do
 		expect(flash[:alert]).to eql(message)
 	end
 
+	let(:user) { FactoryGirl.create(:user)}
+
+	context "standard users" do
+		before do
+			sign_in(user)
+		end
+
+		it "cannot access the new action" do
+			get :new
+
+			expect(response).to redirect_to('/')
+			expect(flash[:alert]).to eql("You must be an admin to do that.")
+    end
+	end
+
+	{ new: :get,
+	  create: :post, 
+	  edit: :get, 
+	  update: :put, 
+	  destroy: :delete }.each do |action, method|
+	  it "cannot access the #{action} action" do
+	  	sign_in(user)
+	  	send(method, action, :id => FactoryGirl.create(:project))
+	  	expect(response).to redirect_to(root_path)
+	  	expect(flash[:alert]).to eql("You must be an admin to do that.")
+	  end
+  end
 end
